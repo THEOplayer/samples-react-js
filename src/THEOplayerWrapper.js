691e0c3bb08cb0f44689c5c15f889d5311416138
Francis Paulin <paulinfrancis@gmail.com>
2020-05-17 15:40:14 +0200

    Use createRef instead of legacy callback ref.

--- src/THEOplayerWrapper.js
+++ src/THEOplayerWrapper.js
@@ -3,15 +3,15 @@ import './THEOplayerWrapper.css';
 
 class Player extends React.Component {
     _player = null;
-    _el = null;
+    _el = React.createRef();
 
     componentDidMount() {
         // props like the source, or the sourcedesciption as a whole can be passed through
         // the can then be used to be called on the player itself
         const { source, onPlay } = this.props;
 
-        if (this._el) {
-            this._player = new window.THEOplayer.Player(this._el, {
+        if (this._el.current) {
+            this._player = new window.THEOplayer.Player(this._el.current, {
                 libraryLocation: "https://cdn.myth.theoplayer.com/7aff3fa6-f92e-45f9-a40e-1bce9911b073/"
             });
 
@@ -27,7 +27,7 @@ class Player extends React.Component {
             className = { "theoplayer-container video-js theoplayer-skin vjs-16-9 THEOplayer" }
             // The ref prop here is key it returns the actual dom element and not the virtual react dom elements
             // Which is need by the player
-            ref = { el => (this._el = el) }
+            ref = { this._el }
             />
         );
     }
