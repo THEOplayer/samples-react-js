eb0681b5df7ced5ae4127a579d5e8dda3c903125
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-05-28 11:55:43 +0200

    Update dependencies

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,16 +1,14 @@
-import { ReportHandler } from 'web-vitals';
+import { MetricType } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-    if (onPerfEntry && onPerfEntry instanceof Function) {
-        import('web-vitals').then(
-            ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-                getCLS(onPerfEntry);
-                getFID(onPerfEntry);
-                getFCP(onPerfEntry);
-                getLCP(onPerfEntry);
-                getTTFB(onPerfEntry);
-            },
-        );
+const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void) => {
+    if (onPerfEntry) {
+        import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+            onCLS(onPerfEntry);
+            onFID(onPerfEntry);
+            onFCP(onPerfEntry);
+            onLCP(onPerfEntry);
+            onTTFB(onPerfEntry);
+        });
     }
 };
 
