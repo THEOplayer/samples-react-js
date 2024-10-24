bac49ae557fcfd730b2804b280f7ca5ca1aad785
George Choustoulakis <george.choustoulakis@theoplayer.com>
2024-10-24 14:04:01 +0200

    Construct view in the same way as our react native package

--- src/player/THEOplayerView.tsx
+++ src/player/THEOplayerView.tsx
@@ -1,48 +1,53 @@
-import { useEffect, useCallback, useState } from 'react';
-import { Player } from 'theoplayer';
+import { useEffect, useCallback, useState, useRef } from 'react';
+import { Player, PlayerConfiguration, UIPlayerConfiguration } from 'theoplayer';
 
 import '/node_modules/theoplayer/ui.css';
 
-export type OnPlayerReady = (player: Player) => void;
+export type PlayerCallback = (player: Player) => void;
 
 interface Props {
-    onPlayer: OnPlayerReady;
+    onPlayerCreated?: PlayerCallback | undefined;
+    onPlayerDestroyed?: PlayerCallback | undefined;
     license: string;
 }
 
-export function THEOplayerView({ onPlayer, license }: Props) {
-    const [player, setPlayer] = useState<Player | null>(null);
-    const onMount = useCallback(
-        (node: HTMLElement | null) => {
-            if (node) {
-                console.log('THEOplayerView, creating THEOplayer');
-                const config = {
-                    license,
-                    // Note: THEOplayer loads workers for transmuxing and other tasks
-                    // these workers need to be located on the same server (see npm run copy:theoplayer)
-                    libraryLocation: `${process.env.PUBLIC_URL}/vendor/theoplayer`,
-                    ui: {
-                        fluid: true,
-                    },
-                };
-
-                setPlayer(new Player(node, config));
-            }
-        },
-        [license],
-    );
+export function THEOplayerView({ onPlayerCreated, license, onPlayerDestroyed }: Props) {
+    const player = useRef<Player | null>(null);
+    const container = useRef<null | HTMLDivElement>(null);
 
     useEffect(() => {
-        if (player && onPlayer) {
-            console.log('THEOplayerView, triggering callback');
-            onPlayer(player);
+        // Create player inside container.
+        if (container.current) {
+            console.log('THEOplayerView, creating THEOplayer');
+            const config: UIPlayerConfiguration = {
+                license,
+                // Note: THEOplayer loads workers for transmuxing and other tasks
+                // these workers need to be located on the same server (see npm run copy:theoplayer)
+                libraryLocation: `${process.env.PUBLIC_URL}/vendor/theoplayer`,
+                ui: {
+                    fluid: true,
+                },
+                mutedAutoplay: 'all'
+            };
+            player.current = new Player(container.current, config);
+            // Notify the player is ready
+            onPlayerCreated?.(player.current);
         }
-    }, [player, onPlayer]);
+
+        // Clean-up
+        return () => {
+            // Notify the player will be destroyed.
+            if (player?.current && onPlayerDestroyed) {
+                onPlayerDestroyed(player?.current);
+            }
+            player?.current?.destroy();
+        };
+    }, [container]);
 
     return (
         <div
+            ref={container}
             className="theoplayer-container video-js theoplayer-skin vjs-16-9 THEOplayer"
-            ref={onMount}
         />
     );
 }
