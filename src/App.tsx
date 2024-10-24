71653a359592973b83efb3620be7996a34b17b49
George Choustoulakis <george.choustoulakis@theoplayer.com>
2024-10-24 14:04:23 +0200

    Update sample app

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
 
     return (
         <div className="App">
-            <THEOplayerView onPlayer={onPlayer} license={license} />
+            <THEOplayerView onPlayerCreated={onPlayer} license={license} />
         </div>
     );
 }
