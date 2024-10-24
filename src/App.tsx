38d690bf408e241c04564c5d74009c53e5723bce
George Choustoulakis <george.choustoulakis@theoplayer.com>
2024-10-24 14:08:56 +0200

    Add sample app

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,90 @@
-import { useCallback } from 'react';
-import { Player } from 'theoplayer';
+import { useCallback, useEffect, useState } from 'react';
+import { Player, SourceDescription } from 'theoplayer';
 import './App.css';
 
 import { THEOplayerView } from './player/THEOplayerView';
 
 const license = '<insert license>';
 
-function App() {
-    const onPlayer = useCallback((player: Player): void => {
-        console.log('onPlayer, loading and setting source');
+const players: Player[] = [];
 
-        player.muted = true;
-        player.autoplay = true;
-        // MPEG-DASH Sample source
-        player.source = {
-            sources: [
-                {
-                    src: '//dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd',
-                    type: 'application/dash+xml',
-                },
-            ],
-        };
-        // HLS Sample source
-        // player.source = {
-        //     sources: [
-        //         {
-        //             src: '//cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny.m3u8',
-        //             type: 'application/x-mpegurl',
-        //         },
-        //     ],
-        // };
+(window as any).players = players
+
+const sources: SourceDescription[] = [
+    {
+        'sources': {
+            'src': 'https://ll-hls-test.cdn-apple.com/llhls4/ll-hls-test-04/multi.m3u8',
+            'type': 'application/x-mpegurl'
+        }
+    },
+    {
+        'sources': {
+            'src': 'https://demo.unified-streaming.com/k8s/live/stable/live.isml/.m3u8',
+            'type': 'application/x-mpegurl'
+        }
+    },{
+        'sources': {
+            'src': 'https://demo.unified-streaming.com/k8s/live/stable/scte35.isml/.m3u8',
+            'type': 'application/x-mpegurl'
+        }
+    }
+];
+
+setInterval(() => {
+    for (const player of players) {
+        // progress the player's currentTime while it's paused
+        if (player.paused && player.seekable.length > 0) {
+            player.currentTime = player.seekable.end(player.seekable.length - 1);
+        }
+    }
+}, 500);
+
+function App() {
+    const [activeSourceId, setActiveSourceId] = useState(0);
+
+    const currentPlayer = activeSourceId % 3;
+
+    console.log(`currentPlayer:${currentPlayer}`)
+
+    const onPlayer = useCallback((player: Player): void => {
+        const initialSourceId = players.length
+        players.push(player);
+        player.preload = 'auto';
+        player.source = sources[initialSourceId];
+        if (initialSourceId === 0) {
+            player.autoplay = true;
+        }
     }, []);
+
+    const nextSource = () => {
+        setActiveSourceId(activeSourceId + 1);
+    }
+
+    useEffect(() => {
+        if (players.length !== 3) {
+            return;
+        }
+        for (let i = 0; i < 3; i++) {
+            if (i === currentPlayer) {
+                players[i].autoplay = true;
+            } else {
+                players[i].pause();
+            }
+        }
+    }, [activeSourceId]);
 
     return (
         <div className="App">
-            <THEOplayerView onPlayerCreated={onPlayer} license={license} />
+            <button onClick={nextSource}>Next source</button>
+            <div style={{ display: currentPlayer === 0 ? '' : 'none' }}>
+                <THEOplayerView onPlayerCreated={onPlayer} license={license} />
+            </div>
+            <div style={{ display: currentPlayer === 1 ? '' : 'none' }}>
+                <THEOplayerView onPlayerCreated={onPlayer} license={license} />
+            </div>
+            <div style={{ display: currentPlayer === 2 ? '' : 'none' }}>
+                <THEOplayerView onPlayerCreated={onPlayer} license={license} />
+            </div>
         </div>
     );
 }
